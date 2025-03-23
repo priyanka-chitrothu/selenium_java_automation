6fb126bdd83d27c7080b1eb7bc8f0c93a0fdfce0
priyank_dev <priyankchitrothu.21@gmail.com>
2025-03-23 22:06:31 +0530

    Added array reverse

--- src/main/java/uat/Test.java
+++ src/main/java/uat/Test.java
@@ -1,4 +1,10 @@
 package uat;
 
 public class Test {
+    public static void main(String[] args){
+        int[] arr ={10,20,30};
+        for(int i= arr.length-1; i>=0; i--){
+            System.out.print(arr[i]);
+        }
+    }
 }
